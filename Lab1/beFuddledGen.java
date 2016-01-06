979c3194c298aa6fbd0ea9502fe70d701ea1432f
takong15 <takong15@gmail.com>
2016-01-05 18:39:23 -0800

    JSON library

---
+++ Lab1/beFuddledGen.java
@@ -0,0 +1,14 @@
+public class beFunddledGen {
+    public static void main(String[] args) {
+    	String outputFilename;
+    	int size = 0;
+    	Scanner scan = new Scanner(System.in);
+
+    	System.out.println("Input output file name: ");
+    	outputFilename = scan.next();
+    	System.out.println("Input number of JSON to generate: ");
+    	size = scan.nextInt();
+
+	}
+}
+
